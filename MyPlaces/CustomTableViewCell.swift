f8655841a2c321870ba30c43b3cb85026615dadc
Artem Fedorchenko <fear.7d@gmail.com>
2020-09-06 01:16:08 +0300

    Custo Taple View Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Artem Fedorchenko on 06.09.2020.
+//  Copyright © 2020 Artem Fedorchenko. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var LocationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
